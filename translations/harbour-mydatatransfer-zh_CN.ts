583cdc711fe1dc35b2f13ff06b124fc7558d8061
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-12-04 09:38:19 +0800

    Create harbour-mydatatransfer-zh_CN.ts

---
+++ translations/harbour-mydatatransfer-zh_CN.ts
@@ -0,0 +1,182 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>Released under the &lt;a href=&apos;https://www.gnu.org/licenses/gpl-3.0&apos;&gt;GNU GPLv3&lt;/a&gt; license.</source>
+        <translation>在 &lt;a href=&apos; 下发布 https://www.gnu.org/licenses/gpl-3.0&apos;&gt; GPLv3 许可协议</translation>
+    </message>
+    <message>
+        <source>Sources</source>
+        <translation>源</translation>
+    </message>
+    <message>
+        <source>Feedback</source>
+        <translation>反馈</translation>
+    </message>
+    <message>
+        <source>If you want to provide feedback or report an issue, please use GitHub.</source>
+        <translation>如果你想提供反馈或报告问题，请使用 Github</translation>
+    </message>
+    <message>
+        <source>Issues</source>
+        <translation>问题</translation>
+    </message>
+    <message>
+        <source>Support</source>
+        <translation>支持</translation>
+    </message>
+    <message>
+        <source>If you like my work and want to buy me a beer, feel free to do it!</source>
+        <translation>如果你喜欢我的工作并且想给我买瓶啤酒，十分欢迎！</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>捐赠</translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation>信誉</translation>
+    </message>
+    <message>
+        <source>Keyboard navigation based on &lt;a href=&apos;https://github.com/Wunderfitz/harbour-piepmatz&apos;&gt;Piepmatz&lt;/a&gt; by Sebastian Wolf.</source>
+        <translation>键盘导航功能基于 &lt;a href=&apos;https://github.com/Wunderfitz/harbour-piepmatz&apos;&gt; Sebasitian Wolf 开发的 Piepmatz&lt;/a&gt; 。</translation>
+    </message>
+    <message>
+        <source>Iconography by</source>
+        <translation>图标制作者为</translation>
+    </message>
+    <message>
+        <source>Thanks to all the testers for being brave and patient.</source>
+        <translation>感谢测试者们的勇敢及耐心。</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>翻译</translation>
+    </message>
+    <message>
+        <source>About My Data Transfer</source>
+        <translation>关于 My Data Transfer</translation>
+    </message>
+    <message>
+        <source>Backup and transfer app data, documents, music, pictures and videos on your Sailfish OS devices.</source>
+        <translation>在旗鱼系统上备份及传输软件数据、文档、音乐、图片及视频。</translation>
+    </message>
+    <message>
+        <source>Thanks to flypigahoy for his ispiring blog post about copying settings and files over a new device.</source>
+        <translation>感谢 Flypigahoy 启发性的关于复制及传输文件到新设备的博客文章。</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Backup</source>
+        <translation>备份</translation>
+    </message>
+    <message>
+        <source>Backuping</source>
+        <translation>正在备份</translation>
+    </message>
+    <message>
+        <source>Restore</source>
+        <translation>恢复</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>无</translation>
+    </message>
+    <message>
+        <source>Select backup</source>
+        <translation>选择备份</translation>
+    </message>
+    <message>
+        <source>Restoring backup</source>
+        <translation>正在恢复备份</translation>
+    </message>
+    <message>
+        <source>My Data Transfer</source>
+        <translation>My Data Transfer</translation>
+    </message>
+    <message>
+        <source>Transfer</source>
+        <translation>Transfer</translation>
+    </message>
+    <message>
+        <source>IP address</source>
+        <translation>IP 地址</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>密码</translation>
+    </message>
+    <message>
+        <source>Transfer to a new device</source>
+        <translation>传输到新设备</translation>
+    </message>
+    <message>
+        <source>Apps</source>
+        <translation>软件</translation>
+    </message>
+    <message>
+        <source>Documents</source>
+        <translation>文档</translation>
+    </message>
+    <message>
+        <source>Downloads</source>
+        <translation>下载</translation>
+    </message>
+    <message>
+        <source>Music</source>
+        <translation>音乐</translation>
+    </message>
+    <message>
+        <source>Pictures</source>
+        <translation>图片</translation>
+    </message>
+    <message>
+        <source>Videos</source>
+        <translation>视频</translation>
+    </message>
+    <message>
+        <source>Choose what to backup. The save file will be stored into your &lt;i&gt;home&lt;/i&gt; directory.</source>
+        <translation>选择要备份的文件。文件会被保存到你的 &lt;i&gt;home&lt;/i&gt;  目录。</translation>
+    </message>
+    <message>
+        <source>Select a save file and choose what to restore.</source>
+        <translation>选择一个保存的文件及要恢复的东西。</translation>
+    </message>
+    <message>
+        <source>Transferring</source>
+        <translation>正在传输</translation>
+    </message>
+    <message>
+        <source>Backup completed</source>
+        <translation>已完成备份</translation>
+    </message>
+    <message>
+        <source>Restore completed</source>
+        <translation>已完成恢复</translation>
+    </message>
+    <message>
+        <source>Transfer completed</source>
+        <translation>已完成传输</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>NOTE: you need the developer mode active and a root password set on your new device in order to be able to use this option.</source>
+        <translation>注意:你需要在你的新设备上打开开发者模式并设置 root 密码以使用该选项。</translation>
+    </message>
+    <message>
+        <source>Insert your new device IP address and password, then choose what to transfer. Both of your devices need to be on the same WLAN network.</source>
+        <translation>输入你新设备的 IP 地址及密码，然后选择要传输的东西。你的设备之间需要连接到相同的 WLAN 网络。</translation>
+    </message>
+</context>
+</TS>
